e3d0f640bb0eedcf38a2c3e241370ab5a35bb88b
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2024-11-04 08:35:39 -0300

    Reformulando app adicionando ViewBinding.

--- app/src/main/java/br/edu/ifsp/dmo1/conversordetemperatura/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/conversordetemperatura/MainActivity.kt
@@ -3,31 +3,20 @@ package br.edu.ifsp.dmo1.conversordetemperatura
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.dmo1.conversordetemperatura.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), OnClickListener {
 
-    private lateinit var editText: EditText
-    private lateinit var button: Button
-    private lateinit var textView: TextView
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        //Buscar as referências dos objetos
-        editText = findViewById(R.id.edit_temperature)
-        button = findViewById(R.id.button_converter)
-        textView = findViewById(R.id.text_fahrenheit)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //Configurar comportamento do botão
-        button.setOnClickListener(this)
+        binding.buttonConverter.setOnClickListener(this)
     }
 
     override fun onClick(view: View) {
@@ -37,9 +26,9 @@ class MainActivity : AppCompatActivity(), OnClickListener {
     }
 
     private fun handleConversion() {
-        val celsius = editText.text.toString().toDouble()
+        val celsius = binding.editTemperature.text.toString().toDouble()
         val fahrenheit = converter(celsius)
-        textView.text = "$fahrenheit ºF"
+        binding.textFahrenheit.text = "$fahrenheit ºF"
     }
 
     private fun converter(celsius: Double): Double {
